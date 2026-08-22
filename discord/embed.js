fde7a8982adcf4d68982c2a4edfff1640a02ce77
Mistral <blongnh@gmail.com>
2026-08-22 17:26:12 +0700

    fix(embed): accurately count Battlepass and contract skins under Battlepass / Standard tier in stats

--- discord/embed.js
+++ discord/embed.js
@@ -1154,13 +1154,18 @@ export const collectionStatsEmbed = async (interaction, id, user) => {
             weaponCounts[skin.weapon] = (weaponCounts[skin.weapon] || 0) + 1;
         }
 
-        switch (skin.rarity) {
-            case "411e4a55-4e59-7757-41f0-86a53f101bb5": tierCounts.ultra++; break;
-            case "e046854e-406c-37f4-6607-19a9ba8426fc": tierCounts.exclusive++; break;
-            case "60bca009-4182-7998-dee7-b8a2558dc369": tierCounts.premium++; break;
-            case "0cebb8be-46d7-c12a-d306-e9907bfc5a25": tierCounts.deluxe++; break;
-            case "12683d76-48d7-84a3-4e09-6985794f0445": tierCounts.select++; break;
-            default: tierCounts.other++; break;
+        // Battlepass, Agent contract, and standard/free skins (unpriced or 1000 VP BP melee)
+        if (!skin.price || skin.price === 1000) {
+            tierCounts.other++;
+        } else {
+            switch (skin.rarity) {
+                case "411e4a55-4e59-7757-41f0-86a53f101bb5": tierCounts.ultra++; break;
+                case "e046854e-406c-37f4-6607-19a9ba8426fc": tierCounts.exclusive++; break;
+                case "60bca009-4182-7998-dee7-b8a2558dc369": tierCounts.premium++; break;
+                case "0cebb8be-46d7-c12a-d306-e9907bfc5a25": tierCounts.deluxe++; break;
+                case "12683d76-48d7-84a3-4e09-6985794f0445": tierCounts.select++; break;
+                default: tierCounts.other++; break;
+            }
         }
     }
 
